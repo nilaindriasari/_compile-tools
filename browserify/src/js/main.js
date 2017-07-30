eab3165d22b0a7aae3662b90917f46e4713c042e
Nila Indriasari <unil.chan@gmail.com>
2017-07-31 11:58:42 +1200

    Browserify example

---
+++ browserify/src/js/main.js
@@ -0,0 +1,9 @@
+const responsiveNav = require('responsive-nav');
+
+const navigation = responsiveNav(".nav-collapse", {
+  transition: 400,
+  insert: 'after'
+});
+
+
+alert('Hey, we try browserify')
